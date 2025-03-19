34a03e856901224050f38f0245a8be8077f83e0a
Yuri Borodavkin <y.a.borodavkin@mail.ru>
2025-03-19 04:29:16 +0300

    Переработано представление предметной области
    добавлен новый класс версий, являющияся предлставлением даты
    Переенесён туда некоторый функционал, относящийся к датам
    
    Перенесён в класс лицензии функционал, относящийся к лицензии
    
    Функции верхнего уровня для генерации массива версий обёрнуты в класс генератора
    
    В следствии ненадобности удалён файл с функционалом

--- interface.rb
+++ interface.rb
@@ -3,20 +3,15 @@
 require 'date'
 require_relative 'functional'
 require_relative 'flussonic_versions_generator'
-
-# last_flussonic_vers = ["24.12", "25.01", "25.02", "25.03"]
+require_relative 'license'
 
 def interface
   puts "Start\n"
-
-  current_date = read_current_date
-  num = read_num
-  last_flussonic_vers = generate_versions_arr(current_date, num)
-  print last_flussonic_vers
-  puts
-  license = make_license
-
-  puts "available_versions: #{find_available_vers(license, last_flussonic_vers)}"
+  last_flussonic_vers = VersionsGenerator.generate_versions
+  puts last_flussonic_vers
+  license = License.new
+  puts 'available_versions:'
+  puts license.available_vers(last_flussonic_vers)
 end
 
 interface
--- license.rb
+++ license.rb
@@ -1,19 +1,44 @@
 # frozen_string_literal: true
 
+require_relative 'version'
+
 # this class contain info about license
 class License
-  def initialize(paid_till, max_version, min_version)
-    @paid_till = paid_till
-    @max_version = max_version
-    @min_version = min_version
+  def initialize
+    puts 'enter paid_till yyyy-mm-dd'
+    @paid_till = Version.new(Version.read_version_in_date_format)
+
+    puts 'enter max_version yy.mm or just press enter and skip'
+    @max_version = Version.new(Version.read_version)
+
+    puts 'enter min_version yy.mm or just press enter and skip'
+    @min_version = Version.new(Version.read_version)
   end
 
   attr_accessor :paid_till, :max_version, :min_version
 
-  #  def to_s
-  #    "license info:\n" \
-  #    "paid till: #{@paid_till}\n" \
-  #    "max_vers: #{@max_version}\n" \
-  #    "min_vers: #{@min_version}\n"
-  #  end
+  def to_s
+    "license info:\n" \
+    "paid till: #{@paid_till}\n" \
+    "max_vers: #{@max_version}\n" \
+    "min_vers: #{@min_version}\n"
+  end
+
+  def self.make_license
+    license = License.new
+    license.read_versions
+    license
+  rescue ArgumentError => e
+    puts "please correct this #{e.message}"
+    retry
+  end
+
+  def available_vers(flussonic_versions)
+    available_vers = flussonic_versions.select do |version|
+      Version.compare_vers(version, @paid_till) <= 0 &&
+        (@max_version.date.nil? || Version.compare_vers(version, @max_version) <= 0) &&
+        (@min_version.date.nil? || Version.compare_vers(version, @min_version) >= 0)
+    end
+    available_vers.empty? ? [@max_version.date] : available_vers
+  end
 end
